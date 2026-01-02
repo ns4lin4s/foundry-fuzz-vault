9a2c1fef2885212640cddf971bcde71c38adbb9a
Nelson Osvaldo Salinas Guajardo <ns4lin4s@gmail.com>
2026-01-02 00:22:19 -0300

    test: add negative tests to cover require statements and reverts

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -22,5 +22,45 @@ contract VaultTest is Test {
         assertEq(vault.totalAssets(), preBalance + amount);
     }
 
+    function testCheckBalance() public {
+        uint256 val = 100;
+        vault.deposit{ value: val }();
+        assert(val == vault.getExternalBalance());
+    }
+
+    function testWithdraw() public {
+        uint256 val = 100;
+        vault.deposit{ value: val }();
+        vault.withdraw(100);
+        assert(vault.getExternalBalance() == 0);
+    }
+
+    function testCanNotWithdraw() public {
+        uint256 val = 2000;
+        vm.expectRevert();
+        vault.withdraw(val);
+    }
+
+    function testDepositZero() public {
+        
+        uint256 deposit = 0;
+        vm.expectRevert();
+        vault.deposit{ value: deposit }();
+    }
+
+    function testGetBalance() public view {
+        assert(vault.getExternalBalance() == 0);
+    }
+
+    function test_RevertIf_WithdrawMoreThanBalance() public {
+        uint256 amount = 1 ether;
+        vm.deal(address(this), amount);
+        
+        vault.deposit{value: amount}();
+        
+        vm.expectRevert();
+        vault.withdraw(amount + 1); // we try withdraw 1 ETH + 1 wei.
+    }
+
     receive() external payable {}
 }
